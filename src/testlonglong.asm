2b6cfe43efb4c46d3e6a04c57eb14de617013673
gianluca <gianlucafrei@gmail.com>
2016-12-05 18:20:52 +0100

    Main grössenteils implementiert

--- src/testlonglong.asm
+++ src/testlonglong.asm
@@ -12,11 +12,29 @@
 ;
 SECTION .bss			; Section containing uninitialized data
 
-lint1: resb 16
-lint2: resb 16
-lint3: resb 16
+X: resb 16
+Y: resb 16
+Z: resb 16
+R: resb 16
+S: resb 16
+T: resb 16
 
 SECTION .data			; Section containing initialised data
+
+MsgX: db "X: "
+MsgY: db "Y: "
+MsgZ: db "Z: "
+MsgR: db "R: "
+MsgS: db "S: "
+MsgT: db "T: "
+MsgLen equ 3
+
+MsgAddXY: db "X = X + Y",10
+MsgAddXYLen equ $-MsgAddXY
+MsgSubXY: db "X = X - Y",10
+MsgSubXYLen equ $-MsgSubXY
+MsgSubRS: db "R = R - S",10
+MsgSubRSLen equ $-MsgSubRS
 
 SECTION .text			; Section containing code
 
@@ -28,22 +46,101 @@ extern subtraction
 
 global 	_start			; Linker needs this to find the entry point!
 
+;This macro prints a string 
+%macro printstr 2
+push rdx
+push rax
+push rdi
+push rsi
+mov rdx, %2          ; lenght of the string
+mov rax, 1           ; Sys write
+mov rdi, 1           ; file descriptor standart-output
+mov rsi, %1  		 ; adress of the buffer
+syscall
+pop rsi
+pop rdi
+pop rax
+pop rdx
+%endmacro
+
+%macro callsub 2
+	mov rdi, %1
+	mov rsi, %2
+	call subtraction
+%endmacro
+%macro calladd 2
+	mov rdi, %1
+	mov rsi, %2
+	call addition
+%endmacro
+%macro callcp 2
+	mov rdi, %1
+	mov rsi, %2
+	call copylonglong
+%endmacro
+%macro callwrite 1
+	mov rdi, %1
+	call writelonglong
+%endmacro
 _start:
 	nop
 
-	mov rdi, lint1
+	;Ask for X
+	printstr MsgX, MsgLen
+	mov rdi, X
+	call readlonglong
+	;Ask for Y
+	printstr MsgY, MsgLen
+	mov rdi, Y
+	call readlonglong
+	;Ask for Z
+	printstr MsgZ, MsgLen
+	mov rdi, Z
 	call readlonglong
 
-	mov rdi, lint2
-	call readlonglong
+	; Copy X to R
+	callcp X, R
+	; Copy Y to S
+	callcp Y, S
+	; Copy Z to T
+	callcp Z, T
 
-	mov rdi, lint1
-	mov rsi, lint2
-	call subtraction
+	;DEBUG: Print R
+	;mov rdi, R
+	;call writelonglong
+	;DEBUG: Print S
+	;mov rdi, S
+	;call writelonglong
+	;DEBUG: Print T
+	;mov rdi, T
+	;call writelonglong
 
-	mov rdi, lint1
-	call writelonglong
+	; X = X + Y
+	calladd X, Y
+	printstr MsgAddXY, MsgAddXYLen
+	printstr MsgX, MsgLen
+	callwrite X
+	printstr MsgY, MsgLen
+	callwrite Y
+	
+	; X = X - Y
+	callsub X, Y
+	printstr MsgSubXY, MsgSubXYLen
+	printstr MsgX, MsgLen
+	printstr MsgX, MsgLen
+	callwrite X
+	printstr MsgY, MsgLen
+	callwrite X
 
+	; R = R - S
+	callsub R, S
+	printstr MsgSubRS, MsgSubRSLen
+	printstr MsgR, MsgLen
+	callwrite R
+	printstr MsgS, MsgLen
+	callwrite S
+
+	;multiplication
 
 	nop
 ; All done! Let's end this party:
